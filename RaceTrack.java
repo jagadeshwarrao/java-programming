f70e33a8c65761aaa880c58f316d2305840cefbb
jagadeshwarrao <60187298+jagadeshwarrao@users.noreply.github.com>
2020-09-04 19:51:00 +0530

    Add files via upload

---
+++ RaceTrack.java
@@ -0,0 +1,13 @@
+import java.util.Scanner; 
+  
+class RaceTrack { 
+    public static void main(String args[]) 
+    { 
+        int a = 11; 
+        int b = 90; 
+        int c = 80; 
+  
+        System.out.println(c * a 
+                           / (c + a - b)); 
+    } 
+} 
